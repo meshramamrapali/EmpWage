21532d0b41ff8547b838cb40f669642afa268b1f
meshramamrapali <amrapalimeshram86@gmail.com>
2023-02-23 19:39:17 +0530

    ADDED : Code to Check Employee Present or Absent

--- EmpWage.java
+++ EmpWage.java
@@ -1,7 +1,13 @@
 package EmployeeWage;
 
 public class EmpWage {
-    public static void main(String[] args){
-        System.out.print("Welcome to EmployeeWage");
+    public static void main(String[] args) {
+        System.out.println("Welcome to EmployeeWage ");
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
     }
 }
